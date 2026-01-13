0a0a9a48db34cf9134cbb45286936c5260c99413
unknown <fikiriatown@gmail.com>
2026-01-13 02:27:58 +0200

    last 3

--- main.py
+++ main.py
@@ -18,6 +18,7 @@ app = FastAPI(title="FastAPI Auth")
 origins = [
     "http://localhost:3000",    
     "http://127.0.0.1:3000",
+    "https://client-gilt-eight.vercel.app",
 ]
 
 app.add_middleware(
